2c150ff34c5df83c565e3569b9eff303b09a6345
Aya-Abouzeid <eng-aya.abouzaid1419@alexu.edu.eg>
2019-04-23 18:16:09 +0200

    Hyper Tuning

--- config.py
+++ config.py
@@ -40,9 +40,9 @@ class TrainingConfig:
     batch_size = 64
     #batch_size = 64
     #batch_size = 32
-    epoch = 5
+    epoch = 6
     iter_to_save = 100
-    sample_size = 64
+    sample_size = 80
     print_batch = True
     drum_filter = np.tile([1,0.3,0,0,0,0.3], 16)
     scale_mask = [1., 0., 1., 0., 1., 1., 0., 1., 0., 1., 0., 1.]
